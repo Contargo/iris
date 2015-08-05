3c23c115c73798012b3744f8e22282b3ebfbf7b3
Tobias Schneider <schneider@synyx.de>
2015-08-05 14:58:07 +0200

    fix api test for route part attributes
    References #11448

--- iris-api-tests/src/test/groovy/connection/EnrichedRouteSpec.groovy
+++ iris-api-tests/src/test/groovy/connection/EnrichedRouteSpec.groovy
@@ -12,27 +12,27 @@ class EnrichedRouteSpec extends Specification {
 
         and: "a route specification"
         def routeParams = [
-                "data.parts[0].origin.longitude": 8.544177,
-                "data.parts[0].origin.latitude": 50.08162,
+                "data.parts[0].origin.longitude"     : 8.544177,
+                "data.parts[0].origin.latitude"      : 50.08162,
                 "data.parts[0].destination.longitude": 8.4232,
-                "data.parts[0].destination.latitude": 49.01179,
-                "data.parts[0].routeType": "TRUCK",
-                "data.parts[0].containerType": "FORTY",
-                "data.parts[0].containerState": "EMPTY",
-                "data.parts[1].origin.longitude": 8.4232,
-                "data.parts[1].origin.latitude": 49.01179,
+                "data.parts[0].destination.latitude" : 49.01179,
+                "data.parts[0].routeType"            : "TRUCK",
+                "data.parts[0].containerType"        : "FORTY",
+                "data.parts[0].containerState"       : "EMPTY",
+                "data.parts[1].origin.longitude"     : 8.4232,
+                "data.parts[1].origin.latitude"      : 49.01179,
                 "data.parts[1].destination.longitude": 8.544177,
-                "data.parts[1].destination.latitude": 50.08162,
-                "data.parts[1].routeType": "TRUCK",
-                "data.parts[1].containerType": "FORTY",
-                "data.parts[1].containerState": "FULL",
-                "data.parts[2].origin.longitude": 8.544177,
-                "data.parts[2].origin.latitude": 50.08162,
+                "data.parts[1].destination.latitude" : 50.08162,
+                "data.parts[1].routeType"            : "TRUCK",
+                "data.parts[1].containerType"        : "FORTY",
+                "data.parts[1].containerState"       : "FULL",
+                "data.parts[2].origin.longitude"     : 8.544177,
+                "data.parts[2].origin.latitude"      : 50.08162,
                 "data.parts[2].destination.longitude": 4.3,
-                "data.parts[2].destination.latitude": 51.36833,
-                "data.parts[2].routeType": "BARGE",
-                "data.parts[2].containerType": "FORTY",
-                "data.parts[2].containerState": "FULL",
+                "data.parts[2].destination.latitude" : 51.36833,
+                "data.parts[2].routeType"            : "BARGE",
+                "data.parts[2].containerType"        : "FORTY",
+                "data.parts[2].containerState"       : "FULL",
         ]
 
         when: "details are requested"
@@ -56,9 +56,9 @@ class EnrichedRouteSpec extends Specification {
             assert it.keySet().size() == 9
             assert it.keySet().containsAll("containerType", "routeType", "direction", "name", "containerState", "origin",
                     "data", "destination", "subRouteParts")
-            assert it.data.keySet().size() == 7
-            assert it.data.keySet().containsAll("electricDistance", "duration", "distance", "dieselDistance",
-                    "tollDistance", "airlineDistance", "co2")
+            assert it.data.keySet().size() == 9
+            assert it.data.keySet().containsAll("electricDistance", "duration", "distance", "railDieselDistance",
+                    "bargeDieselDistance", "co2", "dieselDistance", "tollDistance", "airlineDistance")
         }
     }
 }

6461cc2949bd26093cbc60837ba3ba599830732a
Tobias Schneider <schneider@synyx.de>
2015-08-05 11:52:46 +0200

    Unused function parameters should be removed

--- src/main/webapp/client/js/routing/models/Seaport.js
+++ src/main/webapp/client/js/routing/models/Seaport.js
@@ -1,41 +1,41 @@
 var Seaport = Backbone.Model.extend({
 
-    defaults : {
-        name : "unknown",
-        possibleTypes : undefined,
-        invisible : false
+    defaults: {
+        name: "unknown",
+        possibleTypes: undefined,
+        invisible: false
     },
 
-    initialize : function() {
+    initialize: function () {
         if (this.get("possibleTypes") === undefined) {
             this.set("possibleTypes", []);
         }
     },
 
-    canAcceptTransport : function(transport) {
+    canAcceptTransport: function (transport) {
         // does any of the types support the transport in question?
         return _.any(this.get("possibleTypes"), function (type) {
             return type === transport;
         });
     },
 
-    isInvisible : function() {
+    isInvisible: function () {
         return this.get("invisible");
     }
 });
 
 var SeaportList = SelectableAwareCollection.extend({
 
-    model : Seaport,
+    model: Seaport,
 
-    filterByPossibleType : function(type) {
-        return new SeaportList(this.filter(function(e) {
+    filterByPossibleType: function (type) {
+        return new SeaportList(this.filter(function (e) {
             return e.canAcceptTransport(type);
         }));
     },
 
-    setVisibility : function(type) {
-        this.each(function(e) {
+    setVisibility: function (type) {
+        this.each(function (e) {
             var visible = e.canAcceptTransport(type);
             e.set("invisible", !visible);
         });
@@ -43,26 +43,25 @@ var SeaportList = SelectableAwareCollection.extend({
         this.getDefault().set("defaultValue", true);
     },
 
-    modelSelected : function(e) {
+    modelSelected: function () {
         // override the deselection of other items since seaportlist allows multiple elements to be selected
     },
 
-    setDefaultValue : function(){
-        if(this.size() > 0){
+    setDefaultValue: function () {
+        if (this.size() > 0) {
             this.first().set("defaultValue", "true");
         }
     },
 
-    comparator : function(model){
+    comparator: function (model) {
         return model.get("name");
     },
 
-    getByName : function (name) {
+    getByName: function (name) {
         var selected;
-        this.forEach( function(seaport){
-            if(seaport.get('name') === name){
+        this.forEach(function (seaport) {
+            if (seaport.get('name') === name) {
                 selected = seaport;
-                return;
             }
         });
 

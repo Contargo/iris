c9aa9bc2748e50ffcd53016f3eac21f82b9d69ce
David Schilling <schilling@synyx.de>
2016-02-09 10:55:59 +0100

    Introduce basic auth entry point to prevent browser show up the default login prompt
    References #14307

---
+++ src/main/java/net/contargo/iris/security/IRISBasicAuthEntryPoint.java
@@ -0,0 +1,29 @@
+package net.contargo.iris.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Customized {@code BasicAuthenticationEntryPoint} that overrides {@code BasicAuthenticationEntryPoint#commence} in
+ * order to prevent response header 'WWW-Authenticate' to be set to 'Basic realms=...'. Otherwise browsers would show a
+ * native login prompt.
+ *
+ * @author  David Schilling - schilling@synyx.de
+ */
+public class IRISBasicAuthEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authException) throws IOException, ServletException {
+
+        response.addHeader("WWW-Authenticate", "xBasic realm=\"" + getRealmName() + "\"");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}

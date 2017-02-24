99e45d0f3ac03af56d03c92eb10115ebcda4b55e
Oliver Messner <messner@synyx.de>
2017-02-24 14:06:37 +0100

    Remove CloudDistanceSpec
    Spec removed as API endpoint has been removed with changeset 9803be18

--- iris-api-tests/src/test/groovy/address/staticsearch/StaticAddressByBoundingBoxSpec.groovy
+++ iris-api-tests/src/test/groovy/address/staticsearch/StaticAddressByBoundingBoxSpec.groovy
@@ -6,33 +6,29 @@ import util.ClientFactory
 /**
  * @author Sandra Thieme - thieme@synyx.de
  * @author David Schilling - schilling@synyx.de
+ * @author Oliver Messner - messner@synyx.de
  */
-class StaticAddressByBoundingBoxSpec extends Specification{
+class StaticAddressByBoundingBoxSpec extends Specification {
 
-    def "request for Static Addresses matching the given bounding box"() {
+    def "request for static addresses within a bounding box"() {
 
         given: "a REST client"
         def client = ClientFactory.newAdminClient()
 
         and: "a bounding box definition"
-        def urlParams = [
-                "lat": "49.0126538640",
-                "lon": "8.3770751953",
-                "distance": "10"
-        ]
+        def urlParams = ["lat": "49.0126538640", "lon": "8.3770751953", "distance": "10"]
 
-        when: "static addresses are requested"
+        when: "request static addresses within bounding box"
         def response = client.get(path: "/api/staticaddresses", query: urlParams)
 
         then: "response status code should be 200 (OK)"
-        response.status == 200
+        with(response) {
+            status == 200
 
-        and: "response consists of a list of static addresses"
-        def addresses = response.data
-        addresses.size() == 1
-        addresses[0].size() == 7
-        addresses[0].keySet().containsAll("uniqueId", "geoLocation", "country", "postalcode", "city", "suburb", "hashKey")
-        addresses[0].geoLocation.keySet().containsAll("latitude", "longitude", "type")
-        addresses[0].geoLocation.size() == 3
+            data.first().keySet() == ['uniqueId', 'geoLocation', 'country', 'postalcode',
+                                      'city', 'suburb', 'hashKey'] as Set
+
+            data.first().geoLocation.keySet() == ['latitude', 'longitude', 'type'] as Set
+        }
     }
 }

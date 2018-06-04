0a5ef166efb6d271b6971b755371f9110b4f8537
Ben Antony <antony@synyx.de>
2018-06-04 16:06:33 +0200

    Execute routings in parallel

--- src/main/java/net/contargo/iris/transport/api/TransportApiController.java
+++ src/main/java/net/contargo/iris/transport/api/TransportApiController.java
@@ -6,16 +6,19 @@ import net.contargo.iris.transport.service.DescriptionGenerator;
 import net.contargo.iris.transport.service.TransportDescriptionExtender;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-import static java.util.stream.Collectors.toList;
 
 
 /**
@@ -30,23 +33,37 @@ public class TransportApiController {
 
     private final TransportDescriptionExtender transportDescriptionExtender;
     private final DescriptionGenerator descriptionGenerator;
+    private int maxRoutingThreads;
 
     @Autowired
     public TransportApiController(TransportDescriptionExtender transportDescriptionExtender,
-        DescriptionGenerator descriptionGenerator) {
+        DescriptionGenerator descriptionGenerator,
+        @Value("${routing.threads}") int maxRoutingThreads) {
 
         this.transportDescriptionExtender = transportDescriptionExtender;
         this.descriptionGenerator = descriptionGenerator;
+        this.maxRoutingThreads = maxRoutingThreads;
     }
 
     @RequestMapping(value = "/all", method = POST)
     public List<TransportResponseDto> producePossibleTransportDescriptions(
         @RequestBody TransportTemplateDto template) {
 
-        return descriptionGenerator.from(template)
-            .stream()
-            .map(transportDescriptionExtender::withRoutingInformation)
-            .collect(toList());
+        List<TransportDescriptionDto> descriptions = descriptionGenerator.from(template);
+
+        ExecutorService executor = Executors.newFixedThreadPool(maxRoutingThreads);
+
+        List<TransportResponseDto> result = descriptions.stream()
+                .map(d ->
+                            CompletableFuture.supplyAsync(() ->
+                                    transportDescriptionExtender.withRoutingInformation(d),
+                                executor))
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
+        executor.shutdown();
+
+        return result;
     }
 
 

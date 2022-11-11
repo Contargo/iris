1f3d1a9c488a2faa20819cfff82833a1e7af9a28
Ben Antony <antony@synyx.de>
2022-11-11 14:51:46 +0100

    Drop osrm4 support

--- src/main/java/net/contargo/iris/routing/RoutingQueryStrategyProvider.java
+++ src/main/java/net/contargo/iris/routing/RoutingQueryStrategyProvider.java
@@ -1,6 +1,5 @@
 package net.contargo.iris.routing;
 
-import net.contargo.iris.routing.osrm.Osrm4QueryStrategy;
 import net.contargo.iris.routing.osrm.Osrm5QueryStrategy;
 
 import org.slf4j.Logger;
@@ -12,22 +11,9 @@ import java.lang.invoke.MethodHandles;
 import static org.slf4j.LoggerFactory.getLogger;
 
 
-/**
- * Provides an OSRM {@code RoutingServiceQueryStrategy} applicable for a specific host based on the given logical host
- * name (e.g. "maps1") and a corresponding host specific endpoint URI (e.g. "http://maps1.contargo.net/osrm/viaroute")
- * as defined in the application configuration. If the configured endpoint has its path set to
- * {@link #OSRM_4_ENDPOINT_PATH}, the returned strategy is of type {@link Osrm4QueryStrategy} and if the path is equal
- * to {@code OSRM_5_ENDPOINT_PATH}, a strategy of type {@link Osrm5QueryStrategy} is returned.
- *
- * @author  Oliver Messner - messner@synyx.de
- * @author  David Schilling - schilling@synyx.de
- * @see  RoutingQueryStrategy
- */
 public class RoutingQueryStrategyProvider {
 
     private static final Logger LOG = getLogger(MethodHandles.lookup().lookupClass());
-    private static final String OSRM_4_ENDPOINT_PATH = "/viaroute";
-    private static final String OSRM_5_ENDPOINT_PATH = "/route";
 
     private final RestTemplate restTemplate;
     private final String baseUri;
@@ -49,15 +35,7 @@ public class RoutingQueryStrategyProvider {
      */
     public RoutingQueryStrategy strategy() {
 
-        boolean osrm4Endpoint = baseUri.endsWith(OSRM_4_ENDPOINT_PATH);
-        boolean osrm5Endpoint = baseUri.endsWith(OSRM_5_ENDPOINT_PATH);
-
-        if (!osrm4Endpoint && !osrm5Endpoint) {
-            throw new IllegalArgumentException("No strategy found applicable for endpoint URI " + baseUri);
-        }
-
-        RoutingQueryStrategy strategy = osrm4Endpoint ? new Osrm4QueryStrategy(restTemplate, baseUri)
-                                                      : new Osrm5QueryStrategy(restTemplate, baseUri);
+        RoutingQueryStrategy strategy = new Osrm5QueryStrategy(restTemplate, baseUri);
 
         LOG.debug("Returning a query strategy of type {} applicable endpoint URI {}",
             strategy.getClass().getSimpleName(), baseUri);
--- src/test/java/net/contargo/iris/routing/osrm/RoutingQueryStrategyProviderUnitTest.java
+++ src/test/java/net/contargo/iris/routing/osrm/RoutingQueryStrategyProviderUnitTest.java
@@ -21,30 +21,11 @@ public class RoutingQueryStrategyProviderUnitTest {
     private RoutingQueryStrategyProvider sut;
 
     @Test
-    public void returnsOsrm4QueryStrategy() {
-
-        sut = new RoutingQueryStrategyProvider(restTemplateMock, "http://localhost/osrm/viaroute");
-
-        RoutingQueryStrategy strategy = sut.strategy();
-        assertThat(strategy, instanceOf(Osrm4QueryStrategy.class));
-    }
-
-
-    @Test
     public void returnsOsrm5QueryStrategy() {
 
         sut = new RoutingQueryStrategyProvider(restTemplateMock, "http://localhost/osrm/route");
 
         RoutingQueryStrategy strategy = sut.strategy();
         assertThat(strategy, instanceOf(Osrm5QueryStrategy.class));
-    }
-
-
-    @Test(expected = IllegalArgumentException.class)
-    public void test() {
-
-        sut = new RoutingQueryStrategyProvider(restTemplateMock, "http://localhost/osrm/not-supported");
-
-        sut.strategy();
     }
 }
